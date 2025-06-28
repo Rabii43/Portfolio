57ca012242b3399c705b342784c01d91e13ae3ef
Rabii43 <Rabii12terres@gmail.com>
2025-06-28 17:14:52 +0100

    fix path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,8 +19,8 @@ const Hero = () => {
           <div className="w-40 h-40 rounded-full bg-white/20 backdrop-blur-sm mx-auto mb-6 flex items-center justify-center border-4 border-white/30">
             <div className="w-32 h-32 rounded-full bg-white/30 flex items-center justify-center">
          <img
-              src="./assets/images/terresRabie.png"
-              alt="Rabii Terres - Développeur Full Stack"
+              src="../../assets/images/terresRabie.png" 
+              alt="RT"
               className="w-full h-full object-cover rounded-full"
             />
             </div>
